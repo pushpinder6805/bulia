cdfb3d738dfd237c2d3aeaa70cf7ddc3f03bdc68
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2025-09-30 14:25:51 +0530

    Create top-contributors.js

---
+++ javascripts/discourse/initializers/top-contributors.js
@@ -0,0 +1,35 @@
+import { apiInitializer } from "discourse/lib/api";
+
+export default apiInitializer("1.8.0", (api) => {
+  api.decorateWidget("below-site-header:after", (helper) => {
+    return helper.h("div#top-contributors-container");
+  });
+
+  api.onPageChange(() => {
+    const container = document.querySelector("#top-contributors-container");
+    if (!container || container.dataset.loaded) return;
+
+    fetch("/directory_items.json?period=monthly&order=post_count&limit=5")
+      .then((res) => res.json())
+      .then((data) => {
+        const app = Discourse.__container__.lookup("service:app-events");
+        const topUsers = data.directory_items;
+        app.trigger("top-contributors:loaded", topUsers);
+      });
+
+    container.dataset.loaded = true;
+  });
+
+  api.modifyClass("component:top-contributors-banner", {
+    pluginId: "top-contributors-banner",
+    topUsers: null,
+
+    init() {
+      this._super(...arguments);
+      const app = Discourse.__container__.lookup("service:app-events");
+      app.on("top-contributors:loaded", this, (users) => {
+        this.set("topUsers", users);
+      });
+    }
+  });
+});
